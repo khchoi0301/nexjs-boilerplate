0a56dbb0f7dcd4e378e686c9a0feb04420745e52
khchoi0301 <khchoi0301@gmail.com>
2020-07-23 11:49:56 +0900

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+	return (
+		<div>
+            layout
+			{children}
+		</div>
+	);
+};
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Index = () => {
+	return (
+		<Layout>
+			<div>
+                welcome
+			</div>
+		</Layout>
+	);
+};
+
+export default Index;
