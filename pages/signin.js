5a320110beb596529144967c1ba3cd60a5debc2e
khchoi0301 <khchoi0301@gmail.com>
2020-07-29 14:07:19 +0900

    file name typo 수정

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
-import SignUpForm from "../components/SignUpForm";
+import SignUpForm from "../components/SignupForm";
 
 const SignIn = () => {
 	return (
